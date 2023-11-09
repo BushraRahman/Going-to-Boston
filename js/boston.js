18a7deeb03851c673e2901b05f132b8c9757ddb3
Bushra Rahman <agario4ev@gmail.com>
2023-11-09 14:34:18 -0500

    working on secodn bullet point

--- js/boston.js
+++ js/boston.js
@@ -3,10 +3,17 @@ document.querySelector(".submit").addEventListener("click", function(){
 	rounds = document.querySelector(".textInput").value;
 	document.querySelector(".textInput").disabled = true;
 	document.querySelector(".submit").disabled = true;
-	rollDie()
+	firstPlayer()
 	/* do later: error if rounds is not an odd number*/
 })
 
 let rollDie = () => {
 	console.log(Math.floor(Math.random() * 6)+1)
+}
+
+let firstPlayer = () => {
+	var firstPlayerList = document.getElementsByClassName("firstPlayer").classList
+	for (let i = 0; i < firstPlayerList.length; i++){
+		firstPlayerList[i].classList.remove("hidden")
+	}
 }
